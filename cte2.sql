7178166d4d0e8c146c15acf159d42669a96b5f5d
YOGITA NEGI <116275181+yogi-88@users.noreply.github.com>
2023-09-19 21:41:14 +0100

    Update cte2.sql
    calculated num_link_clicks and num_users

--- cte2.sql
+++ cte2.sql
@@ -1,1 +1,18 @@
+-- Using a CTE and the frontendeventlog tabvle, find the distribution of users across the number of times the email link was clicked per user.
+-- In other words, count the number of users, num_users, in each num_link_clicks category(one clcik, two clicks ,...) using eventid = 5 to track link clicks.
 
+with num_links as (
+    select
+    count(EVENTTIMESTAMP) as num_link_clicks
+from
+    frontendeventlog
+where
+     eventid = 5
+group by userid
+
+)
+
+select
+      num_link_clicks, count(num_link_clicks) as num_users
+from num_links
+group by num_link_clicks;
